b3487987703a869f4ea553c9cb50a5e03aac33b1
Shruti Pawar <shruti.pawar9099@gmail.com>
2024-04-03 12:15:14 +0530

    quotation addition

--- src/Pages/Contactus.jsx
+++ src/Pages/Contactus.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Enquiry from '../Components/Contacts/Enquiry.jsx';
 import Visit from '../Components/Contacts/Visit.jsx';
+import Quotation from '../Components/Contacts/Quotation.jsx';
 
 
 const Contact = () => {
   return (
     <div>
         <Enquiry/>
-        <Visit/>     
+        <Visit/>   
+        <Quotation/>  
     </div>
   );
 }
